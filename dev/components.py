4e173d125b433bc4cae1033e7b31396ecf580977
Simon Koenig <simon.koenig@ait.ac.at>
2024-06-06 16:34:35 +0200

    transferred eval functions to components.py

--- dev/components.py
+++ dev/components.py
@@ -4,6 +4,7 @@ import re
 
 import marqo
 import requests
+from sentence_transformers import SentenceTransformer
 
 
 class VectorStore:
@@ -113,11 +114,16 @@ class VectorStore:
 
     def retrieveDocuments(self, query, k):
         # Retrieve top k documents from indexName based on query
-        return self.mq.index(self.indexName).search(
+        response = self.mq.index(self.indexName).search(
             q=query,  # Query string
             limit=k,  # Number of documents to retrieve
             attributes_to_retrieve=["text", "id"],  # Attributes to retrieve
         )
+
+        contexts = [response["hits"][i]["text"] for i in range(len(response["hits"]))]
+        ids = [response["hits"][i]["id"] for i in range(len(response["hits"]))]
+
+        return contexts, ids
 
     def getBackground(self, query, k):
         # Retrieve top k documents from indexName based on query
@@ -184,19 +190,17 @@ class RagPipe:
         self.PROMPT = userPrompt
         print(f"Prompt set: {userPrompt}")
 
-    def answerQuery(self, query):
-        # Retrieve top k documents from indexName based on query
-
-        background = self.DB.getBackground(query, 3)
-        messages = [
-            {"role": "user", "content": self.PROMPT},
-            {"role": "assistant", "content": background},
-            {"role": "user", "content": query},
-        ]
-
+    def sendToLLM(
+        self,
+        messages,
+        model_temp=0.0,
+        answer_size=100,
+        presence_pen=0.0,
+        repeat_pen=0.0,
+    ):
         headers = {
             "Content-Type": "application/json",
-            "Authorization": f"Bearer N/A ",
+            "Authorization": "Bearer N/A ",
         }
         data = {
             "model": self.LLM_NAME,
@@ -217,9 +221,209 @@ class RagPipe:
                 result = "No result generated!"
         else:
             result = report
-        print("Response: \n")
-        pprint.pprint(result)
         return result
+
+    def evalSendToLLM(
+        self,
+        messages,
+        model_temp=0.0,
+        answer_size=1,
+        presence_pen=0.0,
+        repeat_pen=0.0,
+    ):
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer N/A ",
+        }
+        data = {
+            "model": self.LLM_NAME,
+            "messages": messages,
+            "temperature": model_temp,
+            "max_tokens": answer_size,
+            "presence_penalty": presence_pen,
+            "repeat_penalty": repeat_pen,
+        }
+        endpoint = self.LLM_URL + "/chat/completions"
+        print("Sending query to OpenAI endpoint: " + endpoint)
+        report = requests.post(endpoint, headers=headers, json=data).json()
+        print("Received response...")
+        if "choices" in report:
+            if len(report["choices"]) > 0:  # Always take the first choice.
+                result = report["choices"][0]["message"]["content"]
+            else:
+                result = "No result generated!"
+        else:
+            result = report
+        return result
+
+    def answerQuery(self, query):
+        # Retrieve top k documents from indexName based on query
+
+        background = self.DB.getBackground(query, 3)
+        messages = [
+            {"role": "user", "content": self.PROMPT},
+            {"role": "assistant", "content": background},
+            {"role": "user", "content": query},
+        ]
+
+        result = self.sendToLLM(messages)
+        return result
+
+    def evaluate_context_relevance(
+        self, queries, contexts=None, ids=None, goldPassages=None
+    ):
+        # Type checking
+        if not isinstance(queries, list):
+            raise TypeError(
+                f"Queries must be of type list, but got {type(queries).__name__}."
+            )
+        # check if gold passages available
+        if goldPassages is None:
+            scores = {}
+            for query, context in zip(queries, contexts):
+                if context is None:
+                    context, ids = self.retrieveDocuments(query, 3)
+
+                measurements = []
+                for single_context in context:
+                    # Insert here evaluation measure of retrieved context
+                    # print(f"ID: {single_id}")
+                    print(f"Context: {single_context}")
+                    measure = self.llm_binary_context_relevance(single_context, query)
+                    measurements.append(measure)
+
+                scores[query] = measurements  # Insert evaluation measure here
+
+            return scores
+
+        if goldPassages is not None:
+            pass  # Implement evaluation with goldPassages
+            print("Evaluation with goldPassages not implemented yet")
+
+    def llm_binary_context_relevance(self, context, query):
+        messages = [
+            {
+                "role": "system",
+                "content": "Given the following context and query,"
+                " Give a binary rating, either 0 or 1."
+                " Respond wiht 0 if the context is not sufficient for answering the query. "
+                " Respond with 1 the context is sufficient for answering the query. "
+                'Strictly respond with  either  "0" or "1"'
+                'The output must strictly and only be a single integer "0" or "1" and no additional text.',
+            },
+            {"role": "user", "content": f"Context: {context} ; Query: {query}"},
+        ]
+
+        result = self.evalSendToLLM(messages)
+        return result
+
+    def evaluate_faithfulness(self, answers, contexts):
+        # Type checking
+        if not isinstance(answers, list):
+            raise TypeError(
+                f"Answers must be of type list, but got {type(answers).__name__}."
+            )
+        scores = {}
+        for answer, context in zip(answers, contexts):
+            measurements = []
+            for single_context in context:
+                # Insert here evaluation measure of retrieved context
+                print(f"Context: {single_context}")
+                measure = self.llm_binary_faithfullness(single_context, answer)
+                measurements.append(measure)
+
+            scores[answer] = measurements  # Insert evaluation measure here
+
+        return scores
+
+    def llm_binary_faithfullness(self, context, answer):
+        messages = [
+            {
+                "role": "system",
+                "content": "Given the following context and answer,"
+                " Give a binary rating, either 0 or 1."
+                " Respond wiht 0 if the answer is not sufficiently grounded in the context. "
+                " Respond wiht 1 if the answer is sufficiently grounded in the context. "
+                ' Strictly respond with  either  "0" or "1"'
+                'The output must strictly and only be a single integer "0" or "1" and no additional text.',
+            },
+            {"role": "user", "content": f"Context: {context} ; Answer: {answer}"},
+        ]
+
+        result = self.evalSendToLLM(messages)
+        return result
+
+    def evaluate_answer_relevance(self, queries, answers):
+        # Type checking
+        if not isinstance(answers, list):
+            raise TypeError(
+                f"Answers must be of type list, but got {type(answers).__name__}."
+            )
+
+        if not isinstance(queries, list):
+            raise TypeError(
+                f"Queries must be of type list, but got {type(queries).__name__}."
+            )
+
+        scores = []
+        for answer, query in zip(answers, queries):
+            print(f"Answer: {answer}")
+            print(f"Query: {query}")
+            measure = self.llm_binary_answer_relevance(answer, query)
+            scores.append(measure)
+        return scores
+
+    def llm_binary_answer_relevance(self, answer, query):
+        messages = [
+            {
+                "role": "system",
+                "content": "Given the following query and answer,"
+                "Analyse the question and answer without consulting prior knowledge."
+                " Determine if the answer is relevant to the question."
+                " Give a binary rating, either 0 or 1."
+                " Consider whether the answer addresses all parts of question asked."
+                " Respond with 0 if the answer does not address the question"
+                " Respond with 1 if the answer addresses to the question"
+                ' Strictly respond with  either  "0" or "1"'
+                'The output must strictly and only be a single integer "0" or "1" and no additional text.',
+            },
+            {"role": "user", "content": f"Query: {query} ; Answer: {answer}"},
+        ]
+        result = self.evalSendToLLM(messages)
+        return result
+
+    def evaluate_correctness(self, answers, ground_truths):
+        # Type checking
+        if not isinstance(answers, list):
+            raise TypeError(
+                f"Answers must be of type list, but got {type(answers).__name__}."
+            )
+
+        if not isinstance(ground_truths, list):
+            raise TypeError(
+                f"Queries must be of type list, but got {type(ground_truths).__name__}."
+            )
+
+        scores = []
+        for answer, ground_truth in zip(answers, ground_truths):
+            print(f"Answer: {answer}")
+            print(f"Ground truth: {ground_truth}")
+            measure = self.semantic_similarity(answer, ground_truth)
+            scores.append(measure)
+        return scores
+
+    def semantic_similarity(self, sentence1, sentence2):
+        model = SentenceTransformer("all-mpnet-base-v2")
+
+        sentence1_vec = model.encode([sentence1])
+
+        sentence2_vec = model.encode([sentence2])
+        similarity_score = model.similarity(
+            sentence1_vec, sentence2_vec
+        )  # Default is cosine simi
+        print(f"\n Similarity Score = {similarity_score} ")
+
+        return similarity_score
 
 
 class RagElement:
